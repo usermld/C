d1f8eedd4578e2aa5596dea3bceb4647afaef68e
MU <m.iouriev@gmail.com>
2023-10-05 18:48:03 +0700

    5я задача

---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N. 
+
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2"
+*/
+
+System.Console.WriteLine("Enter number: ");
+
+int number = Convert.ToInt32(Console.ReadLine ());
+int a = -number;
+while(-a < number ) 
+{
+    System.Console.Write(a + " ");
+    a = a + 1;
+} while (true);
+
