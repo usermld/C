4f4e85367ceae8d1a730437458b367f3dce0c92b
MU <m.iouriev@gmail.com>
2023-10-05 17:42:59 +0700

    1я и 2я задача

---
+++ Task00/Program.cs
@@ -0,0 +1,10 @@
+﻿/* 0. Напишите программу, которая на вход принимает число и выдаёт его квадрат 
+(число умноженное на само себя).
+*/
+
+System.Console.WriteLine("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+int result = number * number;
+
+System.Console.WriteLine(result);
---
+++ Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго. 
+
+a = 25; b = 5 -> да
+a = 2 b = 10 -> нет
+a = 9; b = -3 -> да
+a = -3 b = 9 -> нет
+*/
+
+System.Console.WriteLine("Введи числа: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine ());
+int number2 = Convert.ToInt32(Console.ReadLine ());
+
+if (number1 == number2 * number2)
+    {
+        System.Console.WriteLine("yes");
+    }
+else 
+{
+    System.Console.WriteLine("no");
+}
