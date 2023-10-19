69624407dfbdf6df8a3f24ba6807023d14bd686e
MU <m.iouriev@gmail.com>
2023-10-19 21:56:03 +0700

    с дз до 30 (2)

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+double Sqr (int a, int b)
+{
+    double sum = Math.Pow(a, b);
+    return sum;
+}
+
+System.Console.WriteLine("Введите Введите число которое будет возводиться в степень: ");
+int num1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число на которое будет возводится в степень: ");
+int num2 = int.Parse(Console.ReadLine());
+
+double resalt = Sqr(num1, num2);
+System.Console.WriteLine(resalt);
---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int Number(int num)
+{
+    int sum = 0;
+    for (int i = 0; i < num; i++)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+int resalt = Number(num1);
+System.Console.WriteLine($"Сумма цифр в этом числе равна {resalt}.");
