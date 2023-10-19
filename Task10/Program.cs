4b5e0758156a429461512e5904acb1f4ceb09c9f
MU <m.iouriev@gmail.com>
2023-10-19 14:26:12 +0700

    без дз

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿﻿/*
 Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 456 -> 5
 782 -> 8
@@ -23,10 +23,10 @@ else if (number > 100 && number < 1000) {
     System.Console.WriteLine($"2я цифра - {num}");
 }
 else if (number > 1000 && number < 10000) {
-    System.Console.WriteLine("Число 4х значное");
+    System.Console.WriteLine("Число 5и значное");
     int num = number % 1000 / 100;
     System.Console.WriteLine($"2я цифра - {num}");
 }
 else if (number < 0) {
     System.Console.WriteLine("Число меньше нуля");
-}
+} 
---
+++ Task19/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 9999 || number > 100000) {
+    System.Console.WriteLine("Число не пятизначное");
+}
+
+int n1 = number % 10;
+int n2 = number / 10 % 10;
+int n3 = number / 100 % 10;
+int n4 = number / 1000% 10;
+int n5 = number / 10000 % 10;
+
+int num = (n1 * 10000) + (n2 * 1000) + (n3 * 100) + (n4 * 10) + n5; 
+
+if (num == number) {
+    System.Console.WriteLine("True");
+}
+else
+{
+    System.Console.WriteLine("False");
+}
