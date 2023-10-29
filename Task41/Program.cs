ab369c96971c9d094ac571c59cdf96a47cbe015d
MU <m.iouriev@gmail.com>
2023-10-29 21:29:53 +0700

    нет 45го и 43 не правильно, хз почему

---
+++ Task41/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int[] MakeArr(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + ", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+void Comparison (int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) sum += 1;
+    }
+    System.Console.WriteLine(sum);
+}
+
+int size = new Random().Next(0, 13);
+int[] userarr = MakeArr (size);
+PrintArray(userarr);
+System.Console.WriteLine();
+Comparison(userarr);
---
+++ Task43/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+double k1 = 5;
+double b1 = 2;
+
+double k2 = 9;
+double b2 = 4;
+
+System.Console.WriteLine($"{k1}, {b1}, {k2}, {b2}");
+
+
+double LevelsX (double k1, double b1, double k2, double b2)
+{
+    double x = b2 - b1 / k1 - k2;
+
+    return x;
+}
+
+double LevelsY (double b1, double k2, double x)
+{
+    double y = k2 * x + b1;
+
+    return y;
+}
+
+
+double valueX = LevelsX(k1, b1, k2, b2);
+double valueY = LevelsY(b1, k2, valueX);
+System.Console.Write(Math.Round (valueX, 2));
+System.Console.Write("; ");
+System.Console.Write(Math.Round (valueY, 2));
