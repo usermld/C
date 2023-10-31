6afa84a21b1cb6c078939ce71f7fb5bfc7f3547a
MU <m.iouriev@gmail.com>
2023-10-31 10:03:38 +0700

    дз по автотестам без последнего

---
+++ Task47/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+*/ 
+
+/*double[,] Get2DArray (int row, int cow)
+{
+    double[,] array = new double[row, cow];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 10);
+        }
+    }
+    return array;
+}*/
+
+void PrintArray (double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + "\t");
+            if (i == arr.GetLength(0) - 1 && j == arr.GetLength(1) - 1)
+            {
+                System.Console.WriteLine($"m = {i + 1}, n = {j + 1}, minLimitRandom = -10, maxLimitRandom = 10");
+            }
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] CreateRandomMatrix (int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 10);
+        }
+    }
+    return array;
+}
+
+double[,] userArr = CreateRandomMatrix(3,3);
+PrintArray(userArr);
---
+++ Task50/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+50. Напишите программу, которая на вход принимает позиции элемента в 
+двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+1 и 2 -> 2
+1 и 7 -> такого числа в массиве нет
+*/
+
+int[,] Get2DArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Search(int[,] arr, int col, int row)
+{
+    int sCol = 0;
+    int sRow = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == col - 1 && j == row - 1)
+            {
+                System.Console.WriteLine($"{arr[i + 1, j + 1]} - этот элемент в массиве");
+                sCol = i;
+                sRow = j;
+            }
+        }
+    }
+    if (sCol == 0 && sRow == 0)
+    {
+        System.Console.WriteLine("Нет элемента с данным значением");
+    }
+}
+
+int row = new Random().Next(2, 10);
+int col = new Random().Next(0, 10);
+System.Console.WriteLine($"{row} {col} - значения массива");
+
+int row1 = new Random().Next(0, 10);
+int col1 = new Random().Next(0, 10);
+System.Console.WriteLine($"{row1} {col1} - элемент этого массива");
+
+int[,] user2DArray = Get2DArray(row, col); // можно удалить 32 и 33 строчку и в аргумент метода вставить фиксированные значения
+Print2DArray(user2DArray);
+Search(user2DArray, row1, col1);
